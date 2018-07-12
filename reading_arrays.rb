3d3dffee5d6abea07e4278252473106397c7674a
Julia Guillemin <julia.guillemingirls@gmail.com>
2018-07-12 12:33:25 +0000

    automatically backed up by learn

--- reading_arrays.rb
+++ reading_arrays.rb
@@ -11,24 +11,34 @@ def first_student_by_index
   return $student_names[0]
 end
 
-puts first_student_by_index
+first_student_by_index
 
 def fourth_student_by_index
   puts $student_names[4]
 end
 
+fourth_student_by_index
+
 def last_student_by_index
   puts $student_names.room.first
 end
+
+last_student_by_index
 
 def first_student_by_method
  puts $student_names[0]
 end
 
+first_student_by_method
+
 def last_student_by_method
   puts $student_names.first
 end
 
+last_student_by_method
+
 def first_second_and_third_students
   puts $student_names[0,1,2]
 end
+
+first_second_and_third_students
